bbff24547cd30020e14f22bbfcfc309e0ad01199
mobobrov <mobobrov@sberbank.ru>
2025-11-30 22:42:47 +0300

    add my-bank-api-gateway

---
+++ my-bank-api-gateway/src/main/java/ru/yandex/practicum/my/bank/api/gateway/MyBankApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.my.bank.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBankApiGatewayApplication.class, args);
+    }
+}
